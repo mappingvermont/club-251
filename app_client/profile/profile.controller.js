87d713633e2bc70c01900796b4f497eac8d0e59c
mappingvermont <charlie.hofmann@gmail.com>
2016-10-07 10:48:02 -0400

    add user data and popup

--- app_client/profile/profile.controller.js
+++ app_client/profile/profile.controller.js
@@ -1,14 +1,15 @@
 (function() {
-  
-  angular
-    .module('meanApp')
-    .controller('profileCtrl', profileCtrl);
+
+    angular
+        .module('meanApp')
+        .controller('profileCtrl', profileCtrl);
 
 
-  profileCtrl.$inject = ['$http', '$location', '$scope', 'leafletData', 'meanData', 'meanTopoJSON'];
-  function profileCtrl($http, $location, $scope, leafletData, meanData, meanTopoJSON) {
+    profileCtrl.$inject = ['$location', '$scope', '$compile', 'leafletData', 'meanData'];
 
-    angular.extend($scope, {
+    function profileCtrl($location, $scope, $compile, leafletData, meanData) {
+
+        angular.extend($scope, {
             vermont: {
                 lat: 43.9,
                 lng: -72.4,
@@ -16,65 +17,117 @@
             },
             defaults: {
                 scrollWheelZoom: false
-            },
-             markers: {
-                    m1: {
-                        lat: 43.9,
-                        lng: -72.5,
-                        focus: true,
-                        draggable: false,
-                        message: "Hi there!",
-                        icon: {}
-                    }
-                }
+            }
         });
 
 
-    var vm = this;
+        var vm = this;
 
-    vm.user = {};
+        vm.user = {};
 
-    meanData.getProfile()
-      .success(function(data) {
+        meanData.getProfile()
+            .success(function(data) {
 
-        meanTopoJSON.promise.then(function(topojson){
-          console.log('got topojson')
-          joinTopoJson($http, data, topojson, vm, leafletData, $scope)
+                joinTopoJson(data, vm, leafletData)
 
-          });
+            })
+            .error(function(e) {
+                console.log(e);
+            });
 
-      })
-      .error(function (e) {
-        console.log(e);
-      });
+        $scope.messageClick = function() {
+              console.log('popup clicked!');
+            };
 
-  }
+        $scope.$on('leafletDirectiveMap.popupopen', function(event, leafletEvent) {
 
-function joinTopoJson($http, userData, topojson, vm, leafletData, $scope) {
+            console.log('pop up opened!')
 
-        var geometries = topojson.data.objects.towns.geometries;
+            // Create the popup view when is opened
+            var feature = leafletEvent.leafletEvent.popup.options.feature;
 
-        for (i=0; i < geometries.length; i++) {
-          var fips6 = geometries[i].properties.fips6
+            var newScope = $scope.$new();
+            newScope.stream = feature;
 
-          geometries[i].properties.status = userData.towns[fips6];
+            $compile(leafletEvent.leafletEvent.popup._contentNode)(newScope);
+        });
 
-        }
-        console.log('finished join')
-        //userData.topojson = topojson.data
-        userData.topojson = 'placeholder'
+    }
+
+    function joinTopoJson(userData, vm, leafletData) {
 
         leafletData.getMap().then(function(map) {
-          var marker = L.marker($scope.markers.m1)
-          marker.addTo(map);
-        //map.addLayer(markers);
-        //map.fitBounds(markers.getBounds());
-      });
+            var topoJSONlayer = omnivore.topojson('../data/towns.json')
+
+            .on('ready', function() {
+                topoJSONlayer.eachLayer(function(layer) {
+
+                    var fips6 = layer.feature.properties.fips6
+                    layer.feature.properties.status = userData.towns[fips6]
+                    console.log(layer.feature.properties)
+
+                    style_poly(layer)
+
+                    buildPopup(layer.feature, layer);
+
+                });
+            })
+
+            .addTo(map);
+
+        });
 
         vm.user = userData
         console.log(vm.user)
-    
-}
+
+    }
+
+    var styles = {
+        false: '#4daf4a',
+        'FARM': '#e41a1c',
+        'BROOK': '#377eb8',
+        'POND': '#984ea3',
+        'Tie': '#999999',
+        'other': '#ff7f00'
+
+    }
+
+    function style_poly(layer) {
+
+        var style_color = (styles[layer.feature.properties.status] || styles['other']);
+
+        layer.setStyle({
+            color: style_color,
+            fill: style_color,
+            weight: 1,
+        })
+    }
+
+    function buildPopup(feature, layer) {
+
+        var divNode = document.createElement('DIV');
+
+        var popup = "<div class='popup_box_header'><strong>" + feature.properties.town + "</strong></div>";
+        popup += "<hr />";
+
+        popup += '<button type="submit" ng-click="messageClick()"> Click Me!</button><br>'
+
+        var status = feature.properties.status
+
+        popup += status + "<br>";
+
+        divNode.innerHTML = popup
+        layer.bindPopup(divNode, {
+            maxWidth: 450,
+            autoPan: true
+        })
+
+    }
+
+    function messageClick() {
+
+        console.log('clicked')
+    }
 
 
 })();
